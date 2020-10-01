27fa72ed20e2d8e9a541e30d84dd61ec8b5d2a22
Jordan Jordanov <d_yugioh@abv.bg>
2020-10-01 08:54:07 +0300

    03. Data Application

---
+++ 03. Data Application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyDataWebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
