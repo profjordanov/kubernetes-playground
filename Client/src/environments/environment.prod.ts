32df657416f9756732c772603a2c2a7882904435
Jordan Jordanov <d_yugioh@abv.bg>
2020-10-29 09:10:10 +0200

    Initial Commit

---
+++ Client/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  identityApiUrl: '{your-identity-service-IP-and-port}',
+  dealersApiUrl: '{your-dealers-service-IP-and-port}',
+  statisticsApiUrl: '{your-statistics-service-IP-and-port}',
+  notificationsUrl: '{your-notifications-service-IP-and-port}'
+};
